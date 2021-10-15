62026f0a0d3a05db5bae27124f3366e4b3b0819c
Ravenstudios <ravenstudiosrob@yahoo.com>
2021-10-14 21:14:59 -0500

    added sound fx to game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,17 @@ using Random = UnityEngine.Random;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] string catagory;
-    public GameObject prefab;
-    public Transform parent;
-    public float gap;
-    public SpriteRenderer sr;
-    public List<char> letters = new List<char>();
+    [SerializeField] GameObject prefab;
+    [SerializeField] Transform parent;
+    [SerializeField] float gap;
+    [SerializeField] SpriteRenderer sr;
+    [SerializeField] List<char> letters = new List<char>();
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip[] sounds;
+    [SerializeField] float soundFXLevel;
+    [SerializeField] int guessesLeft;
+
+    int numberOfgusses;
 
     //Plants
     string[] plants = { "Belladonna", "Sage", "Lavender", "Rosemary", "Mugwort" };
@@ -29,12 +35,14 @@ public class GameManager : MonoBehaviour
     //Easy
     string[] easy = { "Hallloween", "Pumpkin", "Orange", "Black", "Witch", "Candy", "Trick or Treat", "October" };
 
-
+    string word;
 
     List<GameObject> letterObjects = new List<GameObject>();
 
     void Start()
     {
+
+
         string[][] words = { plants, animales, witchCraft, witchyTools, easy };
 
         int catagoryIndex = Array.IndexOf(words, catagory);
@@ -42,8 +50,11 @@ public class GameManager : MonoBehaviour
 
         string[] randomCatogory = words[randCatagoryndex];
         int randWordIndex = Random.Range(0, randomCatogory.Length);
-        string word = randomCatogory[randWordIndex];
+        word = randomCatogory[randWordIndex];
         word = word.ToUpper();
+
+        word = "CAT";
+        audioSource.Play();
 
         for (int i = 0; i < word.Length; i++)
         {
@@ -92,10 +103,42 @@ public class GameManager : MonoBehaviour
             if (l == letter)
             {
                 letterObjects[i].GetComponent<ShowLetter>().Show(letter);
+                
+                audioSource.PlayOneShot(sounds[1], soundFXLevel);//right awnser
+                numberOfgusses ++;
+
+                if (numberOfgusses == word.Length)
+                {
+                    audioSource.Stop();
+                    audioSource.PlayOneShot(sounds[2], soundFXLevel);//winner
+                    return;
+                }
+
+                return;
+
             }
+           
+           
+
+            
+
+        }
+
+        audioSource.PlayOneShot(sounds[3], soundFXLevel);//wrong awnser
+        guessesLeft--;
+
+        
+
+        if (guessesLeft == 0)
+        {
+            audioSource.Stop();
+            audioSource.PlayOneShot(sounds[0], soundFXLevel);//game over
+            return;
         }
 
     }
+
+
     //compare letters and if match show letter
 }
 
